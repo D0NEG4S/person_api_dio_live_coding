9401e3d501d022aa67ba1934ce37b01ab12440a3
Donegas <marceloff289@gmail.com>
2021-09-02 04:48:38 -0300

    personapi

---
+++ personapi/src/test/java/com/donegas/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donegas.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
